b0aa8e3f6cd8115125be4b557783cf7fbfdbfd6b
m0hamadhassan <m7md.7asn1@gmail.com>
2023-03-17 03:36:42 +0200

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(void)
 {
-char str[]="\"Programming is like building a multilingual puzzle\n";
+char str[]="\"Programming is like building a multilingual puzzle";
 puts(str);
 return 0;
 }
