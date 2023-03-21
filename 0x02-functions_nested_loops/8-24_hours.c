2b21119dd486e39c477c6a3427e9a133842d6b5d
m0hamadhassan <m7md.7asn1@gmail.com>
2023-03-21 23:21:21 +0200

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include"main.h"
+/**
+ * jack_bauer - print minutes
+ * Description: print every minute of day
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar('0' + i / 10);
+			_putchar('0' + i % 10);
+			_putchar(':');
+			_putchar('0' + j / 10);
+			_puthar('0' + j % 10);
+		}
+		_putchar('\n');
+	}
+}
+
