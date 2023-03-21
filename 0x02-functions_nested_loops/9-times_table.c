96b92b809268ec2be767d64119145ccd13893e2b
m0hamadhassan <m7md.7asn1@gmail.com>
2023-03-22 00:47:39 +0200

    commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+ * time_table - print times table
+ * Description: print times table instances
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			_putchar('0' + (i*j) );
+			_putchar('0' + i % 10);
+			_putchar(':');q
+			_putchar('0' + j / 10);
+			_putchar('0' + j % 10);
+			_putchar('\n');
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include<stdio.h>
-#include<stdlib.h>
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,4 +12,9 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-#endif
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif /* MAIN_H */
