8709173f7c64eea7f86add78466dfeaa405c25ad
m0hamadhassan <m7md.7asn1@gmail.com>
2023-03-21 22:39:55 +0200

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,17 +7,11 @@
  *
  */
 int print_last_digit(int n)
-{
+
 	int num;
 
-	if (n < 0)
-	{
-		num = (-1 * n) % 10;
-	}
-	else
-	{
-		num = n % 10;
-	}
+	num = abs(n % 10);
 	_putchar(num + '0');
 	return (num);
 }
+
