543adb60be412f7c6c8a44ee950ae488de572f72
m0hamadhassan <m7md.7asn1@gmail.com>
2023-03-21 22:34:50 +0200

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * _abs - rreturn the absolute value
+ * Description: return the absolute value
+ * @n: input number
+ * Return: the absolute value of input
+ *
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
