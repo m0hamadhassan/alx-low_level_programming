be7d5fe7bc6fe5f6f5565d577ff3d16d71a6be9a
m0hamadhassan <m7md.7asn1@gmail.com>
2023-04-02 01:06:26 +0200

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+ * print_square - print square of '#'
+ * Description: prints square of '#'
+ * @size: square dimensions
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
