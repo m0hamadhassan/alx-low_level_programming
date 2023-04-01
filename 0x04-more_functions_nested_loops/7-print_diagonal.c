ef9b6164a2c3f3115f4b83dffd37dfcc94c849a1
m0hamadhassan <m7md.7asn1@gmail.com>
2023-04-02 00:51:52 +0200

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,11 +16,9 @@ void print_diagonal(int n)
 		{
 			for (j = 0; j < i; j++)
 			{
-				if (i == j)
-				_putchar('\\');
-				else
 				_putchar(' ');
 			}
+			_putchar('\\');
 			_putchar('\n');
 		}
 	}
