259c7183a576c011eb924caedcc7a47fc977329b
m0hamadhassan <m7md.7asn1@gmail.com>
2023-04-07 03:10:57 +0200

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * _print_rev_recursion - print string reversed using recursion 
+ *
+ * Description: function that prints a string, reversed followed by a new line
+ *
+ * @s: pointer to string
+ *
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_puts_recursion(s);
+		_putchar(*s);
+		s++;
+	}
+	else
+		_putchar('\n');
+}
