f4ae05594b75a660b8d03fec6436f5afbc384030
m0hamadhassan <m7md.7asn1@gmail.com>
2023-04-19 17:50:59 +0200

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,15 +3,19 @@
  * array_iterator - array iterator function
  *
  * @array: pointer to array
- * #size: struct of typpe size_t
+ * @size: struct of typpe size_t
  * @action: is a pointer to the function you need to use
  *
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
-	for (i = 0; i < size; i++)
+
+	if (array && action)
 	{
-		action(array[i]);
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
 	}
 }
