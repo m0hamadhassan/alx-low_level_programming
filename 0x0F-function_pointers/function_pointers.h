dbfe785baa99f55cc572ef68f946c9b320ca2a51
m0hamadhassan <m7md.7asn1@gmail.com>
2023-04-21 10:47:30 +0200

    0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
 
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-
+#include <stddef.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
@@ -10,7 +10,7 @@ int int_index(int *array, int size, int (*cmp)(int));
  * enum bool - typedef function for bool
  * @TRUE: 1
  * @FALSE: 0
-*/
+ */
 typedef enum bool
 {
 	TRUE = 1,
